cb8f7540579f73adcde0d2c68d2d964c74cdca1c
zira2801 <131445103+zira2801@users.noreply.github.com>
2026-02-22 01:16:02 -0800

    update: update title UI demo

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Text("Login Screen Demo"),
+      home: const Text("Login Screen Demo đasadawdsdsdsa"),
     );
   }
 }
